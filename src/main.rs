a4a5e24fe9e80b071002fdd1171c26f1b39bf8d8
Fishy <caspar.m.green@gmail.com>
2019-12-31 01:19:26 +0000

    Removed Comments

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,6 @@ fn main() {
     let cooling_stop = 45;
     let mut is_cooling = false;
 
-    // Let's open GPIO23 and -24, e.g. on a Raspberry Pi 2.
-    //let mut gpio23 = gpio::sysfs::SysFsGpioInput::open(23).unwrap();
-    //let mut gpio24 = gpio::sysfs::SysFsGpioOutput::open(24).unwrap();
-
     loop {
         let contents =
             fs::read_to_string(filename).expect("Something went wrong reading the temperature");
